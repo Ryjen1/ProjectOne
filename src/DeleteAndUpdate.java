b2725c0f1c28696eefbd090845ecaf108fe37262
Maryjane Okafor <ryjenspark@gmail.com>
2023-11-12 06:07:00 +0100

    Main.java file deleted

--- src/DeleteAndUpdate.java
+++ src/DeleteAndUpdate.java
@@ -25,7 +25,29 @@ public class DeleteAndUpdate {
         }
     }
     void updateRecord(){
+        try (Connection connect = DriverManager.getConnection(url,myName,myPassword)){
+            String update = "UPDATE students SET email = ? WHERE name = ?";
+            PreparedStatement updateStatement = connect.prepareStatement(update);
+            Scanner scan = new Scanner(System.in);
 
+            System.out.print("Enter the name of student record you want to update");
+            String name1 = scan.nextLine();
+            System.out.print("Enter new email address for student");
+            String email = scan.nextLine();
+
+            updateStatement.setString(1,name1);
+            updateStatement.setString(2,email);
+            updateStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        DeleteAndUpdate deleteOrUpdateRecord = new DeleteAndUpdate();
+        //deleteOrUpdateRecord.deleteRecord();
+        deleteOrUpdateRecord.updateRecord();
     }
 
 }
